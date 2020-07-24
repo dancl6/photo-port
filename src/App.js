4567076423a1ae4a8cc48123867236291d703500
Dan Lyons <danielclyons@yahoo.com>
2020-07-23 20:46:08 -0400

    Sec 20.4.6 update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact'
 
 function App() {
+  
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -17,17 +19,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  
+
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+  categories={categories}
+  setCurrentCategory={setCurrentCategory}
+  currentCategory={currentCategory}
+  contactSelected={contactSelected}
+  setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
